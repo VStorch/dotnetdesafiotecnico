585de0474c2b8f100bb3102051580dd06635dcf4
Vinícius <viniciusstorch62@gmail.com>
2026-05-20 13:38:37 -0300

    chore(back/infra): setup program and startup host with swagger

--- DesafioFullstack/Backend/src/TaskManager.WebApi/Program.cs
+++ DesafioFullstack/Backend/src/TaskManager.WebApi/Program.cs
@@ -1,1 +1,17 @@
-// Entry point
+namespace TaskManager.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- DesafioFullstack/Backend/src/TaskManager.WebApi/Startup.cs
+++ DesafioFullstack/Backend/src/TaskManager.WebApi/Startup.cs
@@ -1,1 +1,42 @@
-// Configuração da API
+namespace TaskManager.WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
